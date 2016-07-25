7b14659c0a0c95204d561bdf6f1a5ad1dbe9ee7e
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-07-25 08:58:58 -0400

    Load RollupJS packaged code (route, template, manifest)

--- nemo_annotator_plugin/__init__.py
+++ nemo_annotator_plugin/__init__.py
@@ -1,5 +1,6 @@
 from flask_nemo.plugin import PluginPrototype
 from pkg_resources import resource_filename
+from flask import url_for, send_from_directory
 """from nemo_oauth_plugin import NemoOauthPlugin"""
 
 
@@ -9,7 +10,9 @@ class AnnotatorPlugin(PluginPrototype):
         "annotator": resource_filename("nemo_annotator_plugin", "data/templates")
     }
 
-    ROUTES = PluginPrototype.ROUTES
+    ROUTES = PluginPrototype.ROUTES + [
+        ("/annotator-assets/<path:filename>", "r_annotator_assets", ["GET"]),
+    ]
 
     def __init__(self, *args, **kwargs):
         super(AnnotatorPlugin, self).__init__(*args, **kwargs)
@@ -19,3 +22,10 @@ class AnnotatorPlugin(PluginPrototype):
         if "template" in kwargs and kwargs["template"] == "main::text.html":
             update["template"] = "annotator::text.html"
         return update
+
+    def r_annotator_assets(self, filename):
+        """ Routes for assets
+        :param filename: Filename in data/assets to retrievee
+        :return: Content of the file
+        """
+        return send_from_directory(resource_filename("nemo_annotator_plugin", "data/assets"), filename)
