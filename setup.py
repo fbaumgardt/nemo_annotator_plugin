cd4b7d3e847f24d292197b9321ea936daf2de3f0
Bridget Almas <balmas@gmail.com>
2016-09-07 10:19:32 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nemo_plokamos_plugin',
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(exclude=["examples", "tests"]),
     url='https://github.com/perseids-project/nemo_plokamos_plugin',
     license='GNU GPL',
