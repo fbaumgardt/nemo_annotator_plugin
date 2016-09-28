2804e36310f9250838d18d3c3d5b2154f10b44ea
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-09-27 21:43:04 -0400

    Version bumped

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nemo_plokamos_plugin',
-    version="0.0.8b",
+    version="0.0.8c0",
     packages=find_packages(exclude=["examples", "tests"]),
     url='https://github.com/perseids-project/nemo_plokamos_plugin',
     license='GNU GPL',
